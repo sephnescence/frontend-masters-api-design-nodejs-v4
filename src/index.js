5dfab003ffe94355211cb6394bad0b41e8e84a47
Blake Taylor <sephnescence@gmail.com>
2023-03-24 22:23:44 +1000

    Set up basic express server

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const app = require('./server')
+
+app.listen(3003, () => {
+    console.log('Server started on https://localhost:3003')
+})
---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('Hello from express')
+    res.status(200) // As opposed to res.statusCode = 200
+    res.json({ // As opposed to res.end()
+        message: 'Hello from express'
+    })
+})
+
+module.exports = app
